63fc19243fb977c350910812e2606d921ed8dc95
Sam Odell <7421902@isd535.org>
2022-04-10 12:02:24 -0700

    worked on truck and formhandler

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+(function (window) {
+    'use strict'
+    
+    let App = window.App;
+    let Truck = App.Truck;
+    let DataStore = App.DataStore;
+
+    let myTruck = new Truck('12345', new DataStore());
+
+    window.myTruck = myTruck;
+
+})(window);
