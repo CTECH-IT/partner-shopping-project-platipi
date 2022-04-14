b6ca6cff285c3262176358c40550b49f675686ea
Sam Odell <7421902@isd535.org>
2022-04-14 10:57:28 -0500

    works

--- scripts/main.js
+++ scripts/main.js
@@ -9,21 +9,21 @@
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
-    let FormHandler = App.FormHandler;
-    let CheckList = App.CheckList;
+    //let FormHandler = App.FormHandler;
+   // let CheckList = App.CheckList;
 
    let myTruck = new Truck('12345', new DataStore());
-    let checkList = new CheckList(CHECKLIST_SELECTOR);
+    //let checkList = new CheckList(CHECKLIST_SELECTOR);
 
     window.myTruck = myTruck;
 
-    let formHandler = new FormHandler(FORM_SELECTOR);
+  //  let formHandler = new FormHandler(FORM_SELECTOR);
 
-    checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+  //  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
 
-    formHandler.addSubmitHandler(function (data) {
+   /* formHandler.addSubmitHandler(function (data) {
         myTruck.createOrder.call(myTruck, data);
         checkList.addRow.call(checkList, data);
-    });
+    });*/
 
 })(window);
