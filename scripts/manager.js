1f38bd03ea8e117f82d0184a8c5b060817aa9549
Sam Odell <7421902@isd535.org>
2022-04-18 11:33:37 -0500

    checklist not getting data store info

--- scripts/manager.js
+++ scripts/manager.js
@@ -10,7 +10,7 @@
     let App = window.App;
     let Cart = App.Cart;
     let DataStore = App.DataStore;
-    let FormHandler = App.FormHandler;
+    //let FormHandler = App.FormHandler;
     let CheckList = App.CheckList;
     let RemoteDataStore = App.RemoteDataStore;
 
@@ -21,13 +21,12 @@
 
     window.myCart = myCart;
 
-   let formHandler = new FormHandler(FORM_SELECTOR);
+    
+
+   //let formHandler = new FormHandler(FORM_SELECTOR);
 
    checkList.addClickHandler(myCart.deliverOrder.bind(myCart));
 
-   formHandler.addSubmitHandler(function (data) {
-        myCart.createOrder.call(myCart, data);
-        checkList.addRow.call(checkList, data);
-    });
+   RemoteDataStore(SERVER_URL);
 
 })(window);
