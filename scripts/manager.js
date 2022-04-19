3985098e59a8f9db38ff073d884dc00207f48f31
Sam Odell <7421902@isd535.org>
2022-04-19 11:05:09 -0500

    working but display is incorrect

--- scripts/manager.js
+++ scripts/manager.js
@@ -6,34 +6,49 @@
     const FORM_SELECTOR = '[data-shopping-order="form"]';
     const CHECKLIST_SELECTOR = '[data-shopping-order="checklist"]';
     const SERVER_URL = 'http://saturn.rochesterschools.org:8080/json';
-    let $ = window.jQuery;
-
 
 
 
     let App = window.App;
     let Cart = App.Cart;
     let DataStore = App.DataStore;
-    //let FormHandler = App.FormHandler;
+    let FormHandler = App.FormHandler;
     let CheckList = App.CheckList;
     let RemoteDataStore = App.RemoteDataStore;
 
     let remoteDS = new RemoteDataStore(SERVER_URL);
 
-    let myCart = remoteDS
-    let checkList = new CheckList(CHECKLIST_SELECTOR);
+    
+
+    let myCart = new Cart('12345', remoteDS);
+    
 
     window.myCart = myCart;
 
-    
+    let checkList = new CheckList(CHECKLIST_SELECTOR);
 
-    
-    
 
-   //let formHandler = new FormHandler(FORM_SELECTOR);
-
-   //ScheckList.addClickHandler(myCart.deliverOrder.bind(myCart));
 
    
+
+   
+  
+    //if (FormHandler !== undefined) {
+       // let formHandler = new FormHandler(FORM_SELECTOR);
+
+       // formHandler.addSubmitHandler(function (data) {
+           // myCart.createOrder.call(myCart, data);
+       // });    
+    if (CheckList !== undefined) {
+        let checkList = new CheckList(CHECKLIST_SELECTOR);
+
+        remoteDS.getAll((orders) => {
+            for (let order of Object.values(orders)) {
+                checkList.addRow(order);
+            }
+        })
+    }
+
+    checkList.addClickHandler(remoteDS.remove.bind(remoteDS));
 
 })(window);
